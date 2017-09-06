97e2f4165cf1901641994101e01ebaa7576995ed
Erin Mahoney <erinmahoneyphoto@gmail.com>
2017-09-06 16:25:08 -0700

    uses env variable

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ const request = require('request');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+require('dotenv').config()
+
 // Priority serve any static files.
 app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
 
@@ -13,7 +15,7 @@ app.get('/api/*', function (req, res) {
   var options = {
     url: 'https://www.sfmoma.org' + req.url,
     headers: {
-      'Authorization': 'Token 53a64b5ecfc68a27eef8668d33322a09897f1bfe'
+      'Authorization': process.env.API_KEYS
     }
   };
 
